e37e21d17104c7e4f38932ebb6b556249c3c93db
Antoine Millet <antoine@inaps.org>
2013-01-29 21:31:52 +0100

    Removed useless comments

--- riddler/urls.py
+++ riddler/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'riddler.views.home', name='home'),
-    # url(r'^riddler/', include('riddler.foo.urls')),
-
     url(r'^test/(?P<test_id>.{4})/$', 'riddler.views.test_index', name='test_index'),
     url(r'^test/(?P<test_id>.{4})/answering$', 'riddler.views.answering_question', name='answering_question'),
     url(r'^test/(?P<test_id>.{4})/(?P<series_id>\d+)/$', 'riddler.views.start_to_answer_series', name='start_to_answer_series'),
