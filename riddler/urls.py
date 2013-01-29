da9388b8262ac5e175dc81de18aa728a3d75d412
Antoine Millet <antoine@inaps.org>
2013-01-29 22:23:32 +0100

    Fixed regex for test_id in urls

--- riddler/urls.py
+++ riddler/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^test/(?P<test_id>.{4})/$', 'riddler.views.test_index', name='test_index'),
-    url(r'^test/(?P<test_id>.{4})/answering$', 'riddler.views.answering_question', name='answering_question'),
-    url(r'^test/(?P<test_id>.{4})/(?P<series_id>\d+)/$', 'riddler.views.start_to_answer_series', name='start_to_answer_series'),
+    url(r'^test/(?P<test_id>.+?)/$', 'riddler.views.test_index', name='test_index'),
+    url(r'^test/(?P<test_id>.+?)/answering$', 'riddler.views.answering_question', name='answering_question'),
+    url(r'^test/(?P<test_id>.+?)/(?P<series_id>\d+)/$', 'riddler.views.start_to_answer_series', name='start_to_answer_series'),
     url(r'^imperavi/', include('imperavi.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
