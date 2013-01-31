f90e72e906be9eac73be4013ad65411218f7ffb2
Antoine Millet <antoine@inaps.org>
2013-01-31 13:52:18 +0100

    Fixed regex in test urls

--- riddler/urls.py
+++ riddler/urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'riddler.views.home', name='home'),
-    url(r'^test/(?P<test_id>.+?)/$', 'riddler.views.test_index', name='test_index'),
-    url(r'^test/(?P<test_id>.+?)/answering$', 'riddler.views.answering_question', name='answering_question'),
-    url(r'^test/(?P<test_id>.+?)/(?P<series_id>\d+)/$', 'riddler.views.start_to_answer_series', name='start_to_answer_series'),
+    url(r'^test/(?P<test_id>[^/]+)/$', 'riddler.views.test_index', name='test_index'),
+    url(r'^test/(?P<test_id>[^/]+)/answering$', 'riddler.views.answering_question', name='answering_question'),
+    url(r'^test/(?P<test_id>[^/]+)/(?P<series_id>\d+)/$', 'riddler.views.start_to_answer_series', name='start_to_answer_series'),
     url(r'^imperavi/', include('imperavi.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
